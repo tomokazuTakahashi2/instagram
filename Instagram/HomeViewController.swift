ddcbf9fc2cd44bbd6ba74d736a54c93402a9f496
tomokazu.takahashi2 <takahashitomokazu4@gmail.com>
2019-08-18 00:09:56 +0900

    Instagramクローン　新規

---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by Raphael on 2019/08/17.
+//  Copyright © 2019 takahashi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
